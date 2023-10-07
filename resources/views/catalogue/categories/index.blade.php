cbf52713ae76e89f078f01b0c45d90a32f82d4ac
Yogesh Salve <yogesh.s@extraswad.com>
2023-10-07 15:01:36 +0530

    category index page

--- resources/views/catalogue/categories/index.blade.php
+++ resources/views/catalogue/categories/index.blade.php
@@ -17,112 +17,161 @@
         width: 100px; /* Set the desired width */
         height: auto; /* Auto-adjust the height to maintain the aspect ratio */
     }
+    .table-row {
+    display: flex;
+    justify-content: space-between; 
+    align-items: center; /* Vertically center the content */
+    }
+    .left-align {
+    text-align: left; /* Left-align the content of the first two td elements */
+    }
+
+    .right-align {
+        text-align: right; /* Right-align the content of the last td */
+    }
 </style>
 
 <br>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <h3>Categories</h3>
-        </div>
-        <div class="col-lg-3 float-right">
-        <input type="text" class="form-control m-1" />
-        </div>
-        <div class="col-lg-3">
-            <button class="btn btn-orange m-1" data-toggle="modal" data-target="#add_category">Add Category</button>
-        </div>
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
     </div>
-    @foreach($category as $category)
-        <div class="card mt-3 p-2">
-            <div style="display: flex; align-items: center;">
-                <div>
-                
+@endif
+
+<div class="card shadow">
+    <div class="card-body d-flex justify-content-between align-items-center">
+        <h3>Categories</h3>        
+        <div class="d-flex align-items-center">
+            <div class="input-group mr-2">                
+                <input type="search" id="searchInput" class="form-control rounded m-1" placeholder="Search by Name" aria-label="Search" aria-describedby="search-addon" />
+                <button type="button" class="btn btn-outline-secondary m-1"><i class="fa fa-filter" aria-hidden="true"></i>&nbsp;Filters</button>           
+                <a href="" class="btn btn-orange m-1" data-toggle="modal" data-target="#add_category">+ Add Category</a>
+            </div>        
+        </div>       
+    </div> 
+</div>
+<br>
+<div class="card">
+<table class="table" id="data-table">
+    <thead></thead>
+        <tbody>
+        <div class="card">
+        @foreach($category as $category)   
+        <tr>
+            <td class="left-align">
                 @if ($category->cat_image)
                     <img src="{{ asset('storage/'.$category->cat_image) }}" alt="Image" class="table-image">
                 @else
                     <img src="{{ asset('storage/item_images/placeholder.png') }}" alt="Placeholder Image" class="table-image">
-                @endif               
-               
-                </div>
-                
-                <div style="margin-left: 10px;">
-                <a href="{{ route('categories.edit', ['id' => $category->id]) }}">{{$category->cat_name}}</h6></a>
-                    
-                    <p>Handle: {{$category->cat_handle}}</p>
-                    <p>{{ $category->items->count() }} items</p>
-                </div>
-                
-            </div>
-        </div>
-    @endforeach
+                @endif
+                &nbsp;
+            </td>
+            <td class="left-align">
+                <a href="{{ route('categories.edit', ['id' => $category->id]) }}">
+                    {{$category->cat_name}}
+                </a><br>
+                <p>Handle: {{$category->cat_handle}}</p>
+            </td>
+            <td class="right-align">
+                {{ $category->items->count() }} items
+            </td>
+        </tr>
+       
+            @endforeach
+        </tbody>
     
-
-
+</table>
+</div>
 
 
 <!-- create Modal -->
-        <div class="modal fade" id="add_category" tabindex="-1" role="dialog" aria-labelledby="addCategoryModal" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-left modal-lg large-modal" role="document">
-        <div class="modal-content p-3">
-            <h5>New Category</h5>
-            <form action="{{ route('categories.store') }}" method="POST">
-                @csrf
-                <div class="row form-row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="cat_name">Name:</label>
-                            <input type="text" name="cat_name" class="form-control" required>
+    <div class="modal fade" id="add_category" tabindex="-1" role="dialog" aria-labelledby="addCategoryModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-left modal-lg large-modal" role="document">
+            <div class="modal-content p-3">
+                <h5>New Category</h5>
+                <form action="{{ route('categories.store') }}" method="POST">
+                    @csrf
+                    <div class="row form-row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="cat_name">Name:</label>
+                                <input type="text" name="cat_name" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="cat_short_name">Short Name:</label>
+                                <input type="text" name="cat_short_name" class="form-control" required>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="cat_short_name">Short Name:</label>
-                            <input type="text" name="cat_short_name" class="form-control" required>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="row form-row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="cat_handle">Handle:</label>
-                            <input type="text" name="cat_handle" class="form-control" required>
+                    <div class="row form-row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="cat_handle">Handle:</label>
+                                <input type="text" name="cat_handle" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="cat_timing_group">Category Timing Group:</label>
+                                <select name="cat_timing_group" class="form-select" aria-label="Default select example">
+                                    <option selected>------</option>
+                                    @foreach($timing as $t)
+                                    <option value="{{$t->id}}">{{$t->name}}</option>
+                                    @endforeach
+                                    
+                                </select>
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
+
+                    
+                    
+                    <div class="row form-row">
                         <div class="form-group">
-                            <label for="cat_timing_group">Category Timing Group:</label>
-                            <select name="cat_timing_group" class="form-select" aria-label="Default select example">
-                                <option selected>------</option>
-                                @foreach($timing as $t)
-                                <option value="{{$t->id}}">{{$t->name}}</option>
-                                @endforeach
-                                
-                            </select>
+                            <label for="cat_desc">Description</label>
+                            <textarea name="cat_desc" class="form-control mt-3" id="exampleFormControlTextarea1" rows="4"></textarea>
                         </div>
                     </div>
-                </div>
 
+                    
                 
-                
-                <div class="row form-row">
-                    <div class="form-group">
-                        <label for="cat_desc">Description</label>
-                        <textarea name="cat_desc" class="form-control mt-3" id="exampleFormControlTextarea1" rows="4"></textarea>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary mt-3" data-dismiss="modal">Cancle</button>
+                    <button type="submit" class="btn btn-orange mt-3">Save</button>
                     </div>
-                </div>
-
-                
-              
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary mt-3" data-dismiss="modal">Cancle</button>
-                <button type="submit" class="btn btn-orange mt-3">Save</button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
+    <script>
+    // Function to filter the table based on user input
+    function filterTable() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("searchInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("data-table");
+        tr = table.getElementsByTagName("tr");
+
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[1]; // Change the index to match the column you want to search
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    }
+
+    // Add an event listener to the search input
+    document.getElementById("searchInput").addEventListener("keyup", filterTable);
+</script>
 
 
 @endsection
