43ca4bfcfdd6e3a5ee1714b69cd49a187d7f9e33
Yogesh Salve <yogesh.s@extraswad.com>
2023-10-10 17:15:24 +0530

    items lable changed

--- resources/views/catalogue/items/index.blade.php
+++ resources/views/catalogue/items/index.blade.php
@@ -157,7 +157,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="exampleModalLabel">Create New Item</h4>                
+                <h4 class="modal-title" id="exampleModalLabel">Create New Items</h4>                
                 </button>
             </div>
         <div class="modal-body"> 
