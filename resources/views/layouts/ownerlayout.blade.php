76ad14c0ed0bdbc4cf66d2064f9b1e71e480e885
4Dranzor <saravananm0406@gmail.com>
2023-10-10 13:55:36 +0530

    Update ownerlayout.blade.php

--- resources/views/layouts/ownerlayout.blade.php
+++ resources/views/layouts/ownerlayout.blade.php
@@ -738,9 +738,9 @@
                     </li> -->
 
                     <li class="nav-item">
-                        <a class="nav-link menu-link" href="#sidebarPages" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarPages">
-                            <i class="bi bi-journal-medical"></i> <span data-key="t-pages">Point of sale</span>
-                        </a>
+                        <!-- <a class="nav-link menu-link" href="#sidebarPages" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarPages">
+                                <i class="bi bi-journal-medical"></i> <span data-key="t-pages">Point of sale</span>
+                            </a> -->
 
                         <div class="collapse menu-dropdown" id="sidebarPages">
                             <ul class="nav nav-sm flex-column">
@@ -755,49 +755,6 @@
                                 </li>
                                 <li class="nav-item">
                                     <a href="#" class="nav-link" data-key="t-starter"> Bill History </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#sidebarProfile" class="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarProfile" data-key="t-profile"> Profile
-                                    </a>
-                                    <div class="collapse menu-dropdown" id="sidebarProfile">
-                                        <ul class="nav nav-sm flex-column">
-                                            <li class="nav-item">
-                                                <a href="pages-profile.html" class="nav-link" data-key="t-simple-page"> Simple Page </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="pages-profile-settings.html" class="nav-link" data-key="t-settings"> Settings </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-team.html" class="nav-link" data-key="t-team"> Team </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-timeline.html" class="nav-link" data-key="t-timeline"> Timeline
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-faqs.html" class="nav-link" data-key="t-faqs"> FAQs </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-pricing.html" class="nav-link" data-key="t-pricing"> Pricing </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-maintenance.html" class="nav-link" data-key="t-maintenance">
-                                        Maintenance
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-coming-soon.html" class="nav-link" data-key="t-coming-soon">
-                                        Coming Soon
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-sitemap.html" class="nav-link" data-key="t-sitemap"> Sitemap </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-search-results.html" class="nav-link" data-key="t-search-results"> Search Results </a>
                                 </li>
                             </ul>
                         </div>
