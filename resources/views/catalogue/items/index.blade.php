2b0861e16b5eca9ffa6f2a7002869da3d8b1791a
4Dranzor <saravananm0406@gmail.com>
2023-10-10 14:45:06 +0530

    Update index.blade.php

--- resources/views/catalogue/items/index.blade.php
+++ resources/views/catalogue/items/index.blade.php
@@ -96,12 +96,12 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Listjs</h4>
+                        <h4 class="mb-sm-0">Items</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
-                                <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
-                                <li class="breadcrumb-item active">Listjs</li>
+                                <!-- <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li> -->
+                                <li class="breadcrumb-item active">Items</li>
                             </ol>
                         </div>
 
@@ -113,9 +113,10 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
-                        <div class="card-header">
+                        <!-- <div class="card-header">
                             <h4 class="card-title mb-0">Add, Edit & Remove</h4>
-                        </div><!-- end card header -->
+                        </div> -->
+                        <!-- end card header -->
 
                         <div class="card-body">
                             <div id="customerList">
