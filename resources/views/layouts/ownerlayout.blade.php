25b808bf159db0c7074b4d95ffa04e228086aa05
4Dranzor <saravananm0406@gmail.com>
2023-10-10 13:53:50 +0530

    Update ownerlayout.blade.php

--- resources/views/layouts/ownerlayout.blade.php
+++ resources/views/layouts/ownerlayout.blade.php
@@ -559,7 +559,7 @@
                         <a href="/owner" class="nav-link menu-link"> <i class="bi bi-speedometer2"></i> <span data-key="t-dashboard">Dashboard</span> </a>
                     </li>
 
-                    <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Pages</span></li>
+                    <li class="menu-title"><i class="ri-more-fill"></i> <span data-key="t-pages">Point of sale</span></li>
 
                     <!-- <li class="nav-item">
                         <a class="nav-link menu-link" href="#sidebarAuth" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarAuth">
@@ -741,56 +741,69 @@
                         <a class="nav-link menu-link" href="#sidebarPages" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarPages">
                             <i class="bi bi-journal-medical"></i> <span data-key="t-pages">Pages</span>
                         </a>
-                        <div class="collapse menu-dropdown" id="sidebarPages">
-                            <ul class="nav nav-sm flex-column">
-                                <li class="nav-item">
-                                    <a href="/billing" class="nav-link" data-key="t-starter"> Billing </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#sidebarProfile" class="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarProfile" data-key="t-profile"> Profile
-                                    </a>
-                                    <div class="collapse menu-dropdown" id="sidebarProfile">
-                                        <ul class="nav nav-sm flex-column">
-                                            <li class="nav-item">
-                                                <a href="pages-profile.html" class="nav-link" data-key="t-simple-page"> Simple Page </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="pages-profile-settings.html" class="nav-link" data-key="t-settings"> Settings </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-team.html" class="nav-link" data-key="t-team"> Team </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-timeline.html" class="nav-link" data-key="t-timeline"> Timeline
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-faqs.html" class="nav-link" data-key="t-faqs"> FAQs </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-pricing.html" class="nav-link" data-key="t-pricing"> Pricing </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-maintenance.html" class="nav-link" data-key="t-maintenance">
-                                        Maintenance
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-coming-soon.html" class="nav-link" data-key="t-coming-soon">
-                                        Coming Soon
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-sitemap.html" class="nav-link" data-key="t-sitemap"> Sitemap </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="pages-search-results.html" class="nav-link" data-key="t-search-results"> Search Results </a>
-                                </li>
-                            </ul>
-                        </div>
+                    <li class="sub"><a href="/dailyregister">Register</a></li>
+                    <li class="sub"><a href="/billing">Billing</a></li>
+                    <li class="sub"><a href="/session">Register Sessions</a></li>
+                    <li class="sub"><a href="#">Bill History</a></li>
+                    <div class="collapse menu-dropdown" id="sidebarPages">
+                        <ul class="nav nav-sm flex-column">
+                            <li class="nav-item">
+                                <a href="/dailyregister" class="nav-link" data-key="t-starter"> Register </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/billing" class="nav-link" data-key="t-starter"> Billing </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="/session" class="nav-link" data-key="t-starter"> Register Sessions </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" data-key="t-starter"> Bill History </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#sidebarProfile" class="nav-link" data-bs-toggle="collapse" role="button" aria-expanded="false" aria-controls="sidebarProfile" data-key="t-profile"> Profile
+                                </a>
+                                <div class="collapse menu-dropdown" id="sidebarProfile">
+                                    <ul class="nav nav-sm flex-column">
+                                        <li class="nav-item">
+                                            <a href="pages-profile.html" class="nav-link" data-key="t-simple-page"> Simple Page </a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a href="pages-profile-settings.html" class="nav-link" data-key="t-settings"> Settings </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-team.html" class="nav-link" data-key="t-team"> Team </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-timeline.html" class="nav-link" data-key="t-timeline"> Timeline
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-faqs.html" class="nav-link" data-key="t-faqs"> FAQs </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-pricing.html" class="nav-link" data-key="t-pricing"> Pricing </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-maintenance.html" class="nav-link" data-key="t-maintenance">
+                                    Maintenance
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-coming-soon.html" class="nav-link" data-key="t-coming-soon">
+                                    Coming Soon
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-sitemap.html" class="nav-link" data-key="t-sitemap"> Sitemap </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="pages-search-results.html" class="nav-link" data-key="t-search-results"> Search Results </a>
+                            </li>
+                        </ul>
+                    </div>
                     </li>
 
 
