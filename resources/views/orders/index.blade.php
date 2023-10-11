d6835c3ebc8f44aa954c9f94d0f518ccd2a832a9
extraaaz <extraaaz@outlook.com>
2023-10-11 20:49:22 +0530

    Done with Billing Task

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -34,19 +34,20 @@
             <!-- end page title -->
 
             <div class="row">
-                <div class="col-sm-6 col-xl-3">
+                <div class="col-sm-6 col-xl-2">
                     <!-- Simple card -->
                     <div class="card">
-                        <ul class="list-group">
+                        <ul class="list-group" id="category-list">
                             @foreach($categoryCounts as $categoryId => $itemCount)
-                            @php
-                            $category = App\Models\Category::find($categoryId);
-                            @endphp
-                            <li class="list-group-item" data-id="{{ $categoryId }}">
-                                {{ $category->cat_name }} ({{ $itemCount }})
-                            </li>
+                                @php
+                                $category = App\Models\Category::find($categoryId);
+                                @endphp
+                                <li class="list-group-item" data-id="{{ $categoryId }}">
+                                    {{ $category->cat_name }} ({{ $itemCount }})
+                                </li>
                             @endforeach
                         </ul>
+                        
 
                     </div><!-- end card -->
                 </div><!-- end col -->
@@ -64,57 +65,13 @@
                         </div>
                         <div class="card-body">
                             <div class="row" id="result">
-                                <div class="col-xl-3 custom-col">
-                                    <div class="card">
-                                        <img class="img-fluid" src="images/small/img-4.jpg" alt="Card image cap">
-                                        <div class="card-body" id="card-body-${itemData.id}">
-                                            <h4 class="card-title mb-2" id="card-title-${itemData.id}">${itemData.item_name}</h4>
-                                            <p class="card-text" id="card-text-${itemData.id}"> Price: ${itemData.item_default_sell_price}</p>
-
-                                        </div>
-                                    </div>
-                                </div><!-- end col -->
-                                <div class="col-xl-3 custom-col">
-                                    <div class="card">
-                                        <img class="img-fluid" src="images/small/img-4.jpg" alt="Card image cap">
-                                        <div class="card-body" id="card-body-${itemData.id}">
-                                            <h4 class="card-title mb-2" id="card-title-${itemData.id}">${itemData.item_name}</h4>
-                                            <p class="card-text" id="card-text-${itemData.id}"> Price: ${itemData.item_default_sell_price}</p>
-
-                                        </div>
-                                    </div>
-                                </div><!-- end col -->
-                                <div class="col-xl-3 custom-col">
-                                    <div class="card">
-                                        <img class="img-fluid" src="images/small/img-4.jpg" alt="Card image cap">
-                                        <div class="card-body" id="card-body-${itemData.id}">
-                                            <h4 class="card-title mb-2" id="card-title-${itemData.id}">${itemData.item_name}</h4>
-                                            <p class="card-text" id="card-text-${itemData.id}"> Price: ${itemData.item_default_sell_price}</p>
-
-                                        </div>
-                                    </div>
-                                </div><!-- end col -->
-                                <div class="col-xl-3 custom-col">
-                                    <div class="card">
-                                        <img class="img-fluid" src="images/small/img-4.jpg" alt="Card image cap">
-                                        <div class="card-body" id="card-body-${itemData.id}">
-                                            <h4 class="card-title mb-2" id="card-title-${itemData.id}">${itemData.item_name}</h4>
-                                            <p class="card-text" id="card-text-${itemData.id}"> Price: ${itemData.item_default_sell_price}</p>
-
-                                        </div>
-                                    </div>
-                                </div><!-- end col -->
-
+                               
                             </div><!-- end row -->
-
-
-
-
                         </div>
                     </div><!-- end card -->
                 </div><!-- end col -->
 
-                <div class="col-sm-6 col-xl-3">
+                <div class="col-sm-6 col-xl-4">
                     <div class="card">
                         <div class="card-body">
 
@@ -128,7 +85,8 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="card" style="height: 300px;">
+                            
+                            <div class="card" id="billdata" style="height: 300px;">
                                 <table class="table">
                                     <thead>
                                         <tr>
@@ -139,25 +97,22 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
-                                            <td>1</td>
-                                            <td>Mark</td>
-                                            <td>Otto</td>
-                                            <td>@mdo</td>
-                                        </tr>
+                                       
                                     </tbody>
                                 </table>
                             </div>
                             <div class="card">
                                 <div class="card-body">
-                                    <table>
+                                    <table id="total">
                                         <tr>
                                             <td>Total :</td>
-                                            <td>6560.00</td>
+                                            <td id="total-amount">0</td>
                                         </tr>
                                     </table>
                                 </div>
                             </div>
+                            
+                            
                             <div class="card">
                                 <div class="card-body">
                                     <button type="button" class="btn btn-primary">Note</button>
@@ -196,47 +151,6 @@
 </div>
 <!-- end main content-->
 
-</div>
-
-
-
-
-
-
-
-
-
-<div class="row">
-    <div class="col-sm-8">
-        <div class="row no-gutters">
-            <div class="col-sm-3">
-                <div class="card shadow" style="background-color: #F5F5F5;">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col">
-                                Categories
-                            </div>
-                            <div class="col-auto">
-                                <button type="button" class="close border-0" aria-label="Close" style="font-size: 20px; height:29px;">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>                    
-                    <ul class="list-group">
-                        @foreach($categoryCounts as $categoryId => $itemCount)
-                        @php
-                        $category = App\Models\Category::find($categoryId);
-                        @endphp
-                        <li class="list-group-item" data-id="{{ $categoryId }}">
-                            {{ $category->cat_name }} ({{ $itemCount }})
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            
-        </div>
 
 
 
@@ -248,82 +162,144 @@
 
 
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var categoryList = document.getElementById('category-list');
+            var resultDiv = document.getElementById('result');
+    
+            // Get the first category by default
+            var defaultCategory = categoryList.querySelector('li.list-group-item');
+    
+            if (defaultCategory) {
+                var defaultCategoryId = defaultCategory.getAttribute('data-id');
+                fetchItemsForCategory(defaultCategoryId);
+            }
+    
+            function fetchItemsForCategory(categoryId) {
+                // Make an AJAX request to the controller with the categoryId
+                fetch('/get-categoryid/' + categoryId, {
+                    method: 'GET',
+                    headers: {
+                        'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then(response => response.json())
+                .then(data => {
+                    var items = data.items;
+                    var cardRow = document.createElement('div');
+cardRow.className = 'row';
 
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                var listItems = document.querySelectorAll('.list-group-item');
+items.forEach(function(itemData) {
+    var cardCol = document.createElement('div');
+    cardCol.className = 'col-xl-3 custom-col';
 
-                listItems.forEach(function(item) {
-                    item.addEventListener('click', function() {
-                        var categoryId = this.getAttribute('data-id');
-                        console.log(categoryId);
+    var card = document.createElement('div');
+    card.className = 'card';
+    card.setAttribute('onclick', `selectItem('${itemData.item_name}', ${itemData.item_default_sell_price})`);
 
-                        // Make an AJAX request to the controller with the categoryId
-                        fetch('/get-categoryid/' + categoryId, {
-                                method: 'GET',
-                                headers: {
-                                    'X-CSRF-TOKEN': '{{ csrf_token() }}',
-                                    'Content-Type': 'application/json',
-                                },
-                            })
-                            .then(response => response.json())
-                            .then(data => {
-                                // Handle the response data and display items in cards
-                                var items = data.items;
-                                var resultDiv = document.getElementById('result');
-                                resultDiv.innerHTML = ''; // Clear the div content
+    var img = document.createElement('img');
+    img.className = 'img-fluid';
+    img.src = 'images/small/img-4.jpg';
+    img.alt = 'Card image cap';
 
-                                var cardRow = document.createElement('p');
-                                cardRow.className = ''; // Create a Bootstrap row
+    var cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    cardBody.id = `card-body-${itemData.id}`;
 
-                                items.forEach(function(itemData) {
-                                    var cardCol = document.createElement('p');
-                                    cardCol.innerHTML = `
-                                        <div class="col-sm-3 custom-col">
-                                        <div class="card">
-                                        <img class="img-fluid" src="images/small/img-4.jpg" alt="Card image cap">
-                                                <div class="card-body" id="card-body-${itemData.id}">
-                                                    <h4 class="card-title mb-2"id="card-title-${itemData.id}">${itemData.item_name}</h4>
-                                                    <p class="card-text"id="card-text-${itemData.id}"> Price: ${itemData.item_default_sell_price}</p>
+    var cardTitle = document.createElement('h4');
+    cardTitle.className = 'card-title mb-2';
+    cardTitle.id = `card-title-${itemData.id}`;
+    cardTitle.textContent = itemData.item_name;
 
-                                                </div>
-                                            </div>
-                                        </div><!-- end col -->
-                    `;
-                                    // <
-                                    //                 div class = "card shadow mb-3 m-1 "
-                                    //                 style = "width:150px;" >
-                                    //                     <
-                                    //                     img src = "http://127.0.0.1:8000/storage/${itemData.item_image}"
-                                    //                 class = "card-img-top"
-                                    //                 alt = "${itemData.item_name}"
-                                    //                 id = "card-img-${itemData.id}" >
-                                    //                     <
-                                    //                     div class = "card-body"
-                                    //                 id = "card-body-${itemData.id}" >
-                                    //                     <
-                                    //                     h6 class = "card-title"
-                                    //                 id = "card-title-${itemData.id}" > $ {
-                                    //                         itemData.item_name
-                                    //                     } < /h6> <
-                                    //                     p class = "card-text"
-                                    //                 id = "card-text-${itemData.id}" > Price: $ {
-                                    //                         itemData.item_default_sell_price
-                                    //                     } < /p> <
-                                    //                     /div> <
-                                    //                     /div>
+    var cardText = document.createElement('p');
+    cardText.className = 'card-text';
+    cardText.id = `card-text-${itemData.id}`;
+    cardText.textContent = `Price: ${itemData.item_default_sell_price}`;
 
-                                    cardRow.appendChild(cardCol);
-                                });
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
 
-                                resultDiv.appendChild(cardRow);
-                            })
-                            .catch(error => {
-                                console.error('Error:', error);
-                            });
-                    });
+    card.appendChild(img);
+    card.appendChild(cardBody);
+    cardCol.appendChild(card);
+    cardRow.appendChild(cardCol);
+});
+
+resultDiv.innerHTML = '';
+resultDiv.appendChild(cardRow);
+
+                })
+                .catch(error => {
+                    console.error('Error:', error);
                 });
+            }
+    
+            categoryList.addEventListener('click', function(event) {
+                var target = event.target;
+                if (target && target.matches('li.list-group-item')) {
+                    var categoryId = target.getAttribute('data-id');
+                    fetchItemsForCategory(categoryId);
+                }
             });
-        </script>
+        });
+    </script>
+    <script>
+        var selectedItems = [];
+    
+        function selectItem(itemName, itemPrice) {
+            var selectedItem = {
+                name: itemName,
+                price: itemPrice,
+                quantity: 1 // Start with a quantity of 1
+            };
+    
+            // Check if the item is already in the selectedItems array
+            var existingItem = selectedItems.find(item => item.name === itemName);
+            
+            if (existingItem) {
+                // If it already exists, increase the quantity
+                existingItem.quantity += 1;
+            } else {
+                // If it's not in the array, add it
+                selectedItems.push(selectedItem);
+            }
+    
+            updateBillData();
+        }
+    
+        function updateBillData() {
+    var billData = document.getElementById('billdata').getElementsByTagName('tbody')[0];
+    billData.innerHTML = ''; // Clear the current content
 
-        @endsection
+    // Initialize the total amount
+    var totalAmount = 0;
+
+    // Add the table header
+    // ...
+
+    // Iterate through selected items and display them in the bill data
+    selectedItems.forEach(item => {
+        var row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item.name}</td>
+            <td>${item.quantity}</td>
+            <td>${item.price}</td>
+            <td>${item.price * item.quantity}</td>
+        `;
+        billData.appendChild(row);
+
+        // Update the total amount
+        totalAmount += item.price * item.quantity;
+    });
+
+    // Display the total amount
+    var totalAmountElement = document.getElementById('total-amount');
+    totalAmountElement.textContent = totalAmount;
+}
+
+
+
+    </script>
+    
+@endsection
