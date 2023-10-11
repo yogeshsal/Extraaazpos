6fc66e312e6c1351d8cd9fbce01566d60479600d
Yogesh Salve <yogesh.s@extraswad.com>
2023-10-11 17:34:58 +0530

    lable

--- resources/views/catalogue/categories/index.blade.php
+++ resources/views/catalogue/categories/index.blade.php
@@ -138,7 +138,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="exampleModalLabel">New Charge</h4>
+                <h4 class="modal-title" id="exampleModalLabel">New Category</h4>
 
                 <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span> -->
