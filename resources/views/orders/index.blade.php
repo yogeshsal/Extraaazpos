8e8bdcf78eac488eff1935006ae3589f0f19919a
4Dranzor <95915135+4Dranzor@users.noreply.github.com>
2023-10-12 19:07:27 +0530

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -113,6 +113,7 @@
                             </div>
                             
                             
+                            
                             <div class="card">
                                 <div class="card-body">
                                     <button type="button" class="btn btn-primary">Note</button>
