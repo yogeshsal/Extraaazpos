e8396dcd16d7e74689562dd832889b619ef32e17
Yogesh Salve <yogesh.s@extraswad.com>
2023-10-12 11:44:48 +0530

    daily register

--- resources/views/dailyregister.blade.php
+++ resources/views/dailyregister.blade.php
@@ -18,13 +18,13 @@
 <div class="main-content">
                 <div class="page-content">
                     <div class="container-fluid">                        
-                        <div class="row">
+                        <!-- <div class="row">
                             <div class="col-12">
                                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                                     <h4 class="mb-sm-0">Daily Register</h4>
                                 </div>
                             </div>
-                        </div> 
+                        </div>  -->
                         <div class="row">
                             <div class="col-xxl-12">
                                 <div class="card">
