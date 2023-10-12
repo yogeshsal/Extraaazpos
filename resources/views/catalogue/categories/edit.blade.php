c8f7823b4260763f96f8f1dab3e0dc185a947ad0
Yogesh Salve <yogesh.s@extraswad.com>
2023-10-13 01:32:58 +0530

    edit category

--- resources/views/catalogue/categories/edit.blade.php
+++ resources/views/catalogue/categories/edit.blade.php
@@ -97,23 +97,25 @@
 
             <div class="row">
                 <div class="col-xxl-12">
-                    <h5 class="mb-3">Edit Taxes</h5>
                     <div class="card">
                         <div class="card-body">
                             <!-- Nav tabs -->
                             <ul class="nav nav-tabs mb-3" role="tablist">
                                 <li class="nav-item ">
-                                    <a class="nav-link active" data-bs-toggle="tab" href="#basicinfo" role="tab" aria-selected="false">
+                                    <a class="nav-link active" data-bs-toggle="tab" href="#basicinfo" role="tab"
+                                        aria-selected="false">
                                         Basic Information
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link " data-bs-toggle="tab" href="#images" role="tab" aria-selected="false">
+                                    <a class="nav-link " data-bs-toggle="tab" href="#images" role="tab"
+                                        aria-selected="false">
                                         Image
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" data-bs-toggle="tab" href="#items" role="tab" aria-selected="false">
+                                    <a class="nav-link" data-bs-toggle="tab" href="#items" role="tab"
+                                        aria-selected="false">
                                         Items ({{$itemCount}})
                                     </a>
                                 </li>
@@ -133,84 +135,110 @@
                                             <div class="col-6">
                                                 <div class="mb-3">
                                                     <label for="NameInput" class="form-label"> Name</label>
-                                                    <input type="text" class="form-control" placeholder="Enter name" name="cat_name" id="compnayNameinput" value="{{ $category->cat_name }}">
+                                                    <input type="text" class="form-control" placeholder="Enter name"
+                                                        name="cat_name" id="compnayNameinput"
+                                                        value="{{ $category->cat_name }}">
                                                 </div>
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
                                             <div class="col-6">
                                                 <div class="mb-3">
                                                     <label for="Description" class="form-label">Short Name</label>
-                                                    <input type="text" class="form-control" name="cat_short_name" placeholder="Enter Description" value="{{ $category->cat_short_name }}" id="phonenumberInput">
+                                                    <input type="text" class="form-control" name="cat_short_name"
+                                                        placeholder="Enter Description"
+                                                        value="{{ $category->cat_short_name }}" id="phonenumberInput">
                                                 </div>
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
                                             <div class="col-6">
                                                 <div class="mb-3">
                                                     <label for="handle" class="form-label">Handle</label>
-                                                    <input type="text" class="form-control" name="cat_handle" value="{{$category->cat_handle}}" placeholder="Enter Handle" id="address1ControlTextarea">
+                                                    <input type="text" class="form-control" name="cat_handle"
+                                                        value="{{$category->cat_handle}}" placeholder="Enter Handle"
+                                                        id="address1ControlTextarea">
                                                 </div>
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
                                             <div class="col-6">
                                                 <div class="mb-3">
-                                                    <label for="cat_timing_group" class="form-label">Timing Group</label>
-                                                    <select name="cat_timing_group" id="cat_timing_group" class="form-select">
+                                                    <label for="cat_timing_group" class="form-label">Timing
+                                                        Group</label>
+                                                    <select name="cat_timing_group" id="cat_timing_group"
+                                                        class="form-select">
                                                         <option selected>Select Timing Group</option>
                                                         @foreach($timing as $t)
-                                                        <option value="{{$t->id}}" @if($t->id == $category->id) selected @endif>
+                                                        <option value="{{$t->id}}" @if($t->id == $category->id) selected
+                                                            @endif>
                                                             {{$t->name}}
                                                         </option>
                                                         @endforeach
                                                     </select>
                                                 </div>
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
                                             <div class="col-6">
                                                 <div class="mb-3">
-                                                    <label for="cat_parent_category" class="form-label">Parent Category</label>
-                                                    <select name="cat_parent_category" id="cat_parent_category" class="form-select">
-                                                        <option value="" disabled selected>Select Parent Category</option>
+                                                    <label for="cat_parent_category" class="form-label">Parent
+                                                        Category</label>
+                                                    <select name="cat_parent_category" id="cat_parent_category"
+                                                        class="form-select">
+                                                        <option value="" disabled selected>Select Parent Category
+                                                        </option>
                                                         @foreach ($categories as $category1)
-                                                        <option value="{{ $category1->id }}" @if($category1->id == $category->id)
+                                                        <option value="{{ $category1->id }}" @if($category1->id ==
+                                                            $category->id)
                                                             selected @endif>
                                                             {{ $category1->cat_name }}
                                                         </option>
                                                         @endforeach
                                                     </select>
                                                 </div>
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
                                             <div class="col-6">
                                                 <div class="mb-3">
                                                     <label for="cat_kot_type" class="form-label">KOT</label>
                                                     <select name="cat_kot_type" id="cat_kot_type" class="form-select">
-                                                        <option value="" disabled selected>Select Applicable Modes</option>
+                                                        <option value="" disabled selected>Select Applicable Modes
+                                                        </option>
                                                         <option value="kot">KOT</option>
                                                         <!-- <option value="in_store">In Store</option> -->
                                                     </select>
                                                 </div>
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
                                             <div class="col-6">
                                                 <!-- Example Textarea -->
                                                 <div>
                                                     <label for="cat_desc" class="form-label">Description</label>
-                                                    <textarea class="form-control" name="cat_desc" id="cat_desc" rows="3"></textarea>
+                                                    <textarea class="form-control" name="cat_desc" id="cat_desc"
+                                                        rows="3"></textarea>
                                                 </div>
                                                 <!-- <div class="mb-3">
                                                     <label for="cat_desc" class="form-label">Description </label>
                                                     <textarea name="cat_desc" id="cat_desc" rows="3"></textarea>
                                                 </div> -->
-                                            </div><!--end col-->
+                                            </div>
+                                            <!--end col-->
 
                                             <div class="col-lg-12">
                                                 <div class="text-end">
                                                     <a href="/taxes">
-                                                        <button type="button" class="btn btn-secondary">Cancel</button></a>
+                                                        <button type="button"
+                                                            class="btn btn-secondary">Cancel</button></a>
                                                     <button type="submit" class="btn btn-orange">Submit</button>
                                                 </div>
-                                            </div><!--end col-->
-                                        </div><!--end row-->
+                                            </div>
+                                            <!--end col-->
+                                        </div>
+                                        <!--end row-->
                                     </form>
 
                                 </div>
                                 <div class="tab-pane " id="images" role="tabpanel">
 
-                                    <form method="POST" action="{{ route('categories.updateImage', $category->id) }}" enctype="multipart/form-data">
+                                    <form method="POST" action="{{ route('categories.updateImage', $category->id) }}"
+                                        enctype="multipart/form-data">
                                         @csrf
                                         @method('PUT')
                                         <div class="form-group form-row">
@@ -230,7 +258,8 @@
                                                 <h4>Associated Items 1</h4>
                                             </div>
                                             <div class="col-auto">
-                                                <a href="{{ route('categories.items', ['id' => $category->id]) }}" type="button" class="btn btn-orange">Update Items</a>  
+                                                <a href="{{ route('categories.items', ['id' => $category->id]) }}"
+                                                    type="button" class="btn btn-orange">Update Items</a>  
                                             </div>
                                         </div>
                                     </div>
@@ -286,7 +315,7 @@
             <div class="row">
                 <div class="col-sm-6">
                     <script>
-                        document.write(new Date().getFullYear());
+                    document.write(new Date().getFullYear());
                     </script>
                     © Hybrix.
                 </div>
@@ -450,28 +479,28 @@
 
 
 <script>
-    $(document).ready(function() {
-        $('#item_is_recommended').bootstrapSwitch();
-        $('#item_is_package_good').bootstrapSwitch();
-        $('#item_sell_by_weight').bootstrapSwitch();
-    });
+$(document).ready(function() {
+    $('#item_is_recommended').bootstrapSwitch();
+    $('#item_is_package_good').bootstrapSwitch();
+    $('#item_sell_by_weight').bootstrapSwitch();
+});
 </script>
 
 <script>
-    $("#content").on("click", ".tabContainer .tabs a", function(e) {
-        e.preventDefault(),
-            $(this)
-            .parents(".tabContainer")
-            .find(".tabContent > div")
-            .each(function() {
-                $(this).hide();
-            });
-
+$("#content").on("click", ".tabContainer .tabs a", function(e) {
+    e.preventDefault(),
         $(this)
-            .parents(".tabs")
-            .find("a")
-            .removeClass("active"),
-            $(this).toggleClass("active"), $("#" + $(this).attr("src")).show();
-    });
+        .parents(".tabContainer")
+        .find(".tabContent > div")
+        .each(function() {
+            $(this).hide();
+        });
+
+    $(this)
+        .parents(".tabs")
+        .find("a")
+        .removeClass("active"),
+        $(this).toggleClass("active"), $("#" + $(this).attr("src")).show();
+});
 </script>
 @endsection
